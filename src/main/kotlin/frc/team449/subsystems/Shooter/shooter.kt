2b0009ef4cf33776ac422c8b188fc452f763dbda
farha <farhanrahaman101@gmail.com>
2026-01-13 16:21:55 -0500

    Shooter/Intake Flywheel 1/13/26

---
+++ src/main/kotlin/frc/team449/subsystems/Shooter/shooter.kt
@@ -0,0 +1,33 @@
+package frc.team449.subsystems.shooter
+
+import au.grapplerobotics.LaserCan
+import au.grapplerobotics.interfaces.LaserCanInterface
+import au.grapplerobotics.simulation.MockLaserCan
+import com.ctre.phoenix6.controls.PositionVoltage
+import com.ctre.phoenix6.hardware.TalonFX
+import dev.doglog.DogLog
+import edu.wpi.first.math.filter.Debouncer
+import edu.wpi.first.wpilibj.RobotBase
+import edu.wpi.first.wpilibj.RobotBase.isSimulation
+import edu.wpi.first.wpilibj2.command.*
+import edu.wpi.first.wpilibj2.command.Commands
+import frc.team449.subsystems.superstructure.intake.IntakeConstants.config
+import frc.team449.system.motor.KrakenDogLog
+import kotlin.math.abs
+
+class Shooter(
+    private val topMotor: TalonFX,
+    private val rightMotor: TalonFX,
+    private val leftMotor: TalonFX,
+    private val backSensor: LaserCanInterface,
+    private val leftSensor: LaserCanInterface,
+    private val rightSensor: LaserCanInterface,
+    private val middleSensor: LaserCanInterface,
+) : SubsystemBase() {
+    private val sensors =
+        listOf(
+            backSensor,
+            leftSensor,
+            rightSensor,
+            middleSensor,
+        )
