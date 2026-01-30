6c85eeaf712266b31a6c97d1823f0d4a213175ce
Edem Setse <edemsetse@gmail.com>
2026-01-30 13:03:37 -0500

    mb

--- src/main/kotlin/frc/team449/subsystems/shooter/ShooterSubsystem.kt
+++ src/main/kotlin/frc/team449/subsystems/shooter/ShooterSubsystem.kt
@@ -1,4 +1,28 @@
 package frc.team449.subsystems.shooter
 
-class ShooterSubsystem {
+import edu.wpi.first.wpilibj2.command.SubsystemBase
+import frc.team449.Constants
+import org.littletonrobotics.junction.Logger
+import org.littletonrobotics.junction.inputs.LoggableInputs
+
+
+class ShooterSubsystem(
+    private val io: ShooterIO
+) : SubsystemBase() {
+    private val inputs: ShooterIO.ShooterIOInputs = ShooterIO.ShooterIOInputs()
+    private var targetPos = 0.0
+
+    override fun periodic() {
+        io.updateInputs(inputs)
+        Logger.processInputs("Climb", inputs as LoggableInputs)
+        Logger.recordOutput("Climb/targetPos", targetPos)
+    }
+
+    fun shoot() {
+        io.run(Constants.ShooterConstants.SHOOTER_VOLTAGE)
+    }
+
+    fun stop() {
+        io.stop()
+    }
 }
