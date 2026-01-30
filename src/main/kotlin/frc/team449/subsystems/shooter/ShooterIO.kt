058f709a0ba0fa9e659a2d16a1826c6006af2d67
Edem Setse <edemsetse@gmail.com>
2026-01-30 13:01:55 -0500

    copy files over part 3

--- src/main/kotlin/frc/team449/subsystems/shooter/ShooterIO.kt
+++ src/main/kotlin/frc/team449/subsystems/shooter/ShooterIO.kt
@@ -1,4 +1,45 @@
 package frc.team449.subsystems.shooter
+import edu.wpi.first.units.measure.Voltage
+import org.littletonrobotics.junction.AutoLog
+import org.littletonrobotics.junction.inputs.LoggableInputs
 
-class ShooterIO {
+interface ShooterIO {
+    @AutoLog
+    open class ShooterIOInputs {
+        @JvmField
+        var leftVoltage: Double = 0.0
+
+        @JvmField
+        var leftSupplyCurrent: Double = 0.0
+
+        @JvmField
+        var leftStatorCurrent: Double = 0.0
+
+        @JvmField
+        var leftTemperature: Double = 0.0
+
+        @JvmField
+        var leftMotorIsConnected: Boolean = false
+
+        @JvmField
+        var rightVoltage: Double = 0.0
+
+        @JvmField
+        var rightSupplyCurrent: Double = 0.0
+
+        @JvmField
+        var rightStatorCurrent: Double = 0.0
+
+        @JvmField
+        var rightTemperature: Double = 0.0
+
+        @JvmField
+        var rightMotorIsConnected: Boolean = false
+    }
+
+    fun updateInputs(inputs: ShooterIOInputs) {}
+
+    fun run(voltage: Double) {}
+
+    fun stop() {}
 }
