9838016e3a8113e835264b18ae761882adfa28b9
Edem Setse <edemsetse@gmail.com>
2026-01-30 13:03:02 -0500

    copy files over part 5

--- src/main/kotlin/frc/team449/subsystems/shooter/ShooterIOSim.kt
+++ src/main/kotlin/frc/team449/subsystems/shooter/ShooterIOSim.kt
@@ -1,4 +1,17 @@
 package frc.team449.subsystems.shooter
+import edu.wpi.first.math.controller.PIDController
+import edu.wpi.first.math.system.plant.DCMotor
+import edu.wpi.first.wpilibj.simulation.ElevatorSim
 
-class ShooterIOSim {
+class ShooterIOSim : ShooterIO {
+
+    //lots to do
+
+    override fun run(position: Double) {
+        //set voltage
+    }
+
+    override fun stop() {
+        //stop voltage
+    }
 }
