edcc020eba0aa0b08d5a0e20d94d4bf5c47ee180
theKnightedEgg <jefferydoudou@gmail.com>
2026-02-26 08:51:26 -0500

    happy ktlint made

--- src/main/kotlin/frc/team449/Bindings.kt
+++ src/main/kotlin/frc/team449/Bindings.kt
@@ -1,9 +1,6 @@
 package frc.team449
 
-import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.CommandScheduler
-import edu.wpi.first.wpilibj2.command.Commands
-import edu.wpi.first.wpilibj2.command.Commands.runOnce
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup
 import edu.wpi.first.wpilibj2.command.button.Trigger
