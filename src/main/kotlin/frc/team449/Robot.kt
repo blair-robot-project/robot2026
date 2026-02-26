321dbd3e7d3e52b11f741957bbd43aa917ce392f
jamesp <james20902@gmail.com>
2026-02-26 11:23:11 -0500

    Revert "vision subsystem implementation (#6)"
    This reverts commit b43a087af65f7b784b23e5a721f16cd483cfd10a.

--- src/main/kotlin/frc/team449/Robot.kt
+++ src/main/kotlin/frc/team449/Robot.kt
@@ -90,7 +90,9 @@ class Robot : LoggedRobot() {
 
     override fun testPeriodic() {}
 
-    override fun simulationInit() {}
+    override fun simulationInit() {
+        Logger.recordOutput("ZeroedComponentPoses", *Array(3) { Pose3d() })
+    }
 
     override fun simulationPeriodic() {
         Logger.recordOutput(
